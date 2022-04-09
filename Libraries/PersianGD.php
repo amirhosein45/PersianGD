8195d19fbb52d0af2cbd2943cc896e165e1647a9
amirhossein2020 <ah.haghighat80@gmail.com>
2022-04-09 13:23:55 +0430

    Add files via upload

---
+++ Libraries/PersianGD.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Libraries;
+
+
+class PersianGD {
+
+    public function persian_log2vis(&$str){
+
+    include_once('Bidi.php');
+    
+    $bidi = new bidi();
+    
+    $text = explode("\n", $str);
+    
+    $str = array();
+    
+    foreach($text as $line){
+        $chars = $bidi->utf8Bidi($bidi->UTF8StringToArray($line), 'AL');
+        $line = '';
+        foreach($chars as $char){
+            $line .= $bidi->unichr($char);
+        }
+        
+        $str[] = $line;
+    }
+    
+    $str = implode("\n", $str);
+    return $str;
+}
+
+}
+
+
+?>
